bae4ede97cd7d76c84e6bfb645acbcc05c46fc47
Jonathan Fisher <jonathan.fshr@gmail.com>
2021-09-04 14:26:29 +0200

    Add project files.

---
+++ Class/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    class Car
+    {
+        //Blue print for a car(Bilmärke, pris/dag, total milage)
+        public string CarName;
+        public float PricePerDay;
+        public float CarMilage;
+        public float Revenue;
+
+        //Constructor
+        public Car() { }
+
+        public Car(string _CarName, float _PricePerDay, float _TotalMilage)
+        {
+            CarName = _CarName;
+            PricePerDay = _PricePerDay;
+            CarMilage = _TotalMilage;
+        }
+        Firma r = new Firma();
+        //method to take the car out
+        public void RentCar(int days, float milesPerday)
+        {
+            Revenue += days * PricePerDay;
+            CarMilage += days * milesPerday;
+            r.TotalRevenue += Revenue;
+        }
+
+        public override string ToString()
+        {
+            return $"This {CarName} has run {CarMilage} kilometers and generated {Revenue} kr.";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ClassMethods;
+using System;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW", 3000, 4);
+            Car car2 = new Car("Toyota", 2000, 10);
+            Car car3 = new Car("Audi", 5000, 6);
+
+            Firma rental1 = new Firma();
+            Firma rental2 = new Firma();
+
+            rental1.AvailableCars.Add(car1);
+            rental1.AvailableCars.Add(car2);
+            
+            rental2.AvailableCars.Add(car3);
+
+            car1.RentCar(3, 3);
+            car3.RentCar(1, 5);
+
+            Console.WriteLine("Total revenue\nRental 1: " + rental1.TotalRevenue + "kr\nRental 2: " + rental2.TotalRevenue + "kr");
+            
+            //foreach (var item in rental1.AvailableCars)
+            //{
+            //    Console.WriteLine(item.CarName);
+            //}
+            
+           
+
+            
+
+           
+
+
+
+            
+        }
+    }
+}
